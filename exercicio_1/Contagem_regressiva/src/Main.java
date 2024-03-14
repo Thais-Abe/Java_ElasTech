ecb6102b08eb12d00a4e2b19e7fa621385d34d99
Thais <esx.tabe@terceiros.mag.com.br>
2024-03-14 15:11:38 -0300

    subindo primeiro exercício

---
+++ exercicio_1/Contagem_regressiva/src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 10;
+      for(int i = number; i > 0; i--){
+          System.out.println(i);
+      }
+    }
+}
